74b6a70c0f4c0f95148b24833e22eb3ea26ef60b
AnaghaRw <anagha.r.warrier@gmail.com>
2024-04-13 21:51:49 +0530

    about pg started

--- script.js
+++ script.js
@@ -30,3 +30,18 @@ function extractText(letter) {
 window.onload = function () {
     extractTextAfterTransition();
 };
+
+const navEl= document.querySelector(".navbar");
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 56){
+        navEl.classList.add('navbar-scrolled');
+        navEl.classList.remove('bg-transparent');
+    }
+
+    else if(window.scrollY < 56){
+        navEl.classList.remove('navbar-scrolled');
+        navEl.classList.add('bg-transparent');
+
+    }
+});
